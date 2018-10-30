9d75a3e41a7b211a33667a70af7369facaac0c0a
Thong The Le <lethe.thong.it@gmail.com>
2018-10-30 17:55:54 +0300

    Add http server...

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/thongthele/romanserver/numerals"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	http.HandleFunc("/api/v1/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		if paths[3] == "roman-number" {
+			number, _ := strconv.Atoi(paths[4])
+			if number > 0 && number <= 10 {
+				fmt.Fprintf(w, "%q", html.EscapeString(numerals.Numbers[number]))
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("404 - Not Found"))
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Bad Request"))
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
